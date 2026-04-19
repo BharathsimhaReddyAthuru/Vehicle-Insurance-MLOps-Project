0e9bf9c45df0dc925d5c7c22a4c95a2516be87b4
bgonward23 <bharathathuru97@gmail.com>
2026-04-19 18:14:40 +0200

    project structure ready, venv set

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="src",
+    version="0.0.1",
+    author="Bharath",
+    author_email="bharath.athuru97@gmail.com",
+    packages=find_packages()
+)
